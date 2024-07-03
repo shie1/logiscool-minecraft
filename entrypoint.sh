d76ad2972c14702017a379acd83ca3e0f3fedaba
Shie1 <shie.1@outlook.com>
2024-07-03 11:33:46 +0200

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Please do not change the envvars after the first launch, as it will not be reflected in the server.properties file."
-java -Xmx2048 -Xms2048 -jar paper-server_1.12.2.jar nogui
+RAM=${RAM:-1024M}
+java -Xmx$RAM -Xms$RAM -jar paper-server_1.12.2.jar nogui
