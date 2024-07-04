7d97ee6ec7aefa27e849075821fc6f9ca269801b
Shie1 <shie.1@outlook.com>
2024-07-04 10:47:01 +0200

    fix?

--- generate_config.sh
+++ generate_config.sh
@@ -8,8 +8,8 @@ echo "Generating server.properties file..."
 echo "server.port=25565" > server.properties
 echo "motd=Logiscool Tábor szerver" >> server.properties
 echo "server-ip=" >> server.properties
-printf 'max-players=%s\n' "$MAXPLAYERS" >> server.properties
-printf 'online-mode=%s\n' "$ONLINEMODE" >> server.properties
+echo "max-players=$MAXPLAYERS" >> server.properties
+echo "online-mode=$ONLINEMODE" >> server.properties
 echo "gamemode=1" >> server.properties
 echo "allow-flight=true" >> server.properties
 echo "enable-command-block=false" >> server.properties
