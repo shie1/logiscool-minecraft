4ee668599da273942f4f2063f7e7e85a1b01a80c
Shie1 <shie.1@outlook.com>
2024-07-04 10:00:28 +0200

    log config because something is wrong

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "Please do not change the envvars after the first launch, as it will not be reflected in the server.properties file."
+
+echo "======================"
+echo "Current configuration:"
+echo $(cat server.properties)
+echo "======================"
+
 RAM=${RAM:-1024M}
 java -Xmx$RAM -Xms$RAM -jar paper-server_1.12.2.jar nogui
